a89f9d01691ea6ec06d2111c3271137190c2dd7f
Vale Marset <valemarset05@gmail.com>
2022-07-26 15:47:04 -0300

    Resolución de la segunda parte del ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Sumar(10,20,35));
+
+        // Llamada a la función Sumar pasandole tres parámetros.
+        Sumar(10,20,35);
+
+        // Creación del objeto "miCoche"
+        Coche miCoche = new Coche();
+
+        // Llamada a la función IncrementarPuertas() desde el objeto "miCoche"
+        miCoche.IncrementarPuertas();
+
+        //Impresión en consola de la cantidad de puertas que tiene el objeto "miCoche" (1)
+        System.out.println(miCoche.numPuertas);
+
     }
 
 
-
+ // Función que suma tres números:
     public static int Sumar (int a, int b, int c){
 
         return (a + b + c);
@@ -12,10 +24,26 @@ public class Main {
     }
 
 
+}
+
+//Creación de la clase Coche
+class Coche {
+
+    //Declaración e inicialización de la variable numPuertas
+    public int numPuertas = 0;
+
+    //Definición de la función IncrementarPuertas()
+    public void IncrementarPuertas(){
 
 
+        this.numPuertas ++;
 
-
+    }
 
 }
+
+
+
+
+
 
