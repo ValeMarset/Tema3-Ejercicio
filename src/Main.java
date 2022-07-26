51f736cf3642b23fa11f4b5baa5650778eff6c4a
Vale Marset <valemarset05@gmail.com>
2022-07-26 15:13:57 -0300

    Primera parte del ejercicio resuelta

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Sumar(10,20,35));
+    }
+
+
+
+    public static int Sumar (int a, int b, int c){
+
+        return (a + b + c);
+
+    }
+
+
+
+
+
+
+
+}
+
